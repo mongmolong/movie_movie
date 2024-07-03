0a795a5baf06f4a57e104cff89223fab9909254d
mongmolong <nisatang@naver.com>
2024-07-03 19:27:35 +0900

    retry

--- src/router copy.js
+++ src/router copy.js
@@ -9,5 +9,24 @@ const router = createBrowserRouter([
     { path: '/', element: <MovieList /> },
     { path: '/tv', element: <TvList /> },
     { path: '/:id', element: <MovieDetail /> },
+
+    
+
+    // {
+    //     path: "/",
+    //     element: <MovieList />
+    // },
+    // {
+    //     path: "/movies/:id", // 영화 상세 페이지
+    //     element: <MovieDetail />
+    // },
+    // {
+    //     path: "/tv",
+    //     element: <TvList />
+    // },
+    // {
+    //     path: "/tv/:id", // TV 프로그램 상세 페이지
+    //     element: <TvDetails />
+    // },
 ]);
 export default router;
--- src/router.js
+++ src/router.js
@@ -6,45 +6,28 @@ import TvDetails from "./components/TvDetails";
 
 
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+    [
+        {
+            path: "/",
+            element: <MovieList />
+        },
+        {
+            path: "/:id", //params를 통해 접근가능
+            element: <MovieDetail />
+        },
+        {
+            path: "/tv",
+            element: <TvList />
+        },
+        {
+            path: "/:id", //params를 통해 접근가능
+            element: <TvDetails />
+        }],
     {
-        path: "/",
-        element: <MovieList />
-    },
-    {
-        path: "/:id", //params를 통해 접근가능
-        element: <MovieDetail />
-    },
-    {
-        path: "/tv",
-        element: <TvList />
-    },
-    {
-        path: "/:id", //params를 통해 접근가능
-        element: <TvDetails />
-    },
-    {
-      basename: process.env.PUBLIC_URL
+        basename: process.env.PUBLIC_URL
     }
 
-    
-
-    // {
-    //     path: "/",
-    //     element: <MovieList />
-    // },
-    // {
-    //     path: "/movies/:id", // 영화 상세 페이지
-    //     element: <MovieDetail />
-    // },
-    // {
-    //     path: "/tv",
-    //     element: <TvList />
-    // },
-    // {
-    //     path: "/tv/:id", // TV 프로그램 상세 페이지
-    //     element: <TvDetails />
-    // },
-])
+);
 
 export default router;
